9d354a8fe000ca13a6f8f76f4d33f497842c3bf5
Yan Kairalla <yanmkairalla@hotmail.com>
2025-08-08 03:21:25 -0300

    Initial commit

---
+++ src/main/java/com/shorturl/shortUrl/controller/ShortUrlController.java
@@ -0,0 +1,19 @@
+package com.shorturl.shortUrl.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShortUrlController {
+
+    @PostMapping("/shorten")
+    public void createShortUrl(@RequestBody String url){
+
+    }
+    @GetMapping("/{shortUrl}")
+    public void getShortUrl(@RequestBody String shortUrl){
+
+    }
+}
