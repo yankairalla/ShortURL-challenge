6b955d441d7159ebc150ae8b14e369ff6ea78dcc
Yan Kairalla <yanmkairalla@hotmail.com>
2025-08-16 06:46:36 -0300

    WIP:insere e verifica se é a url é valida

--- src/main/java/com/shorturl/shortUrl/controller/ShortUrlController.java
+++ src/main/java/com/shorturl/shortUrl/controller/ShortUrlController.java
@@ -1,5 +1,9 @@
 package com.shorturl.shortUrl.controller;
 
+import com.shorturl.shortUrl.model.ShortenURL;
+import com.shorturl.shortUrl.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ShortUrlController {
 
+    @Autowired
+    private UrlService urlService;
+
+
     @PostMapping("/shorten")
-    public void createShortUrl(@RequestBody String url){
-        
+    public ResponseEntity<ShortenURL> createShortUrl(@RequestBody String url){
+        ShortenURL shortenURL = urlService.createShortUrl(url);
+        return ResponseEntity.ok(shortenURL);
     }
     @GetMapping("/{shortUrl}")
     public void getShortUrl(@RequestBody String shortUrl){
