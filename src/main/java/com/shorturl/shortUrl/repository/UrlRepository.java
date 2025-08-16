36c9f7c61c5e77c0e65b667719e09a1c339ce52b
Yan Kairalla <yanmkairalla@hotmail.com>
2025-08-16 06:44:42 -0300

    Adiciona procura pela url original

--- src/main/java/com/shorturl/shortUrl/repository/UrlRepository.java
+++ src/main/java/com/shorturl/shortUrl/repository/UrlRepository.java
@@ -2,6 +2,11 @@ package com.shorturl.shortUrl.repository;
 
 import com.shorturl.shortUrl.model.ShortenURL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UrlRepository extends JpaRepository<ShortenURL, Long> {
+   Optional<ShortenURL> findBySourceUrl(String url);
 }
